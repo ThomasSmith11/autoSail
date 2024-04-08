e59ff1f236ab0b51a10e8afcb6a3a73b69068dfc
ThomasSmith11 <wts205393@gmail.com>
2024-04-07 22:43:46 -0400

    add target heading calculation function

--- autoSail.ino
+++ autoSail.ino
@@ -1,8 +1,11 @@
+#include <cmath>
 #include <Servo.h>
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 #include <Wire.h> //maybe not needed if compass doesn't work
 #include <HMC5883L_Simple.h> //maybe not needed if compass doesn't work
+
+using namespace std;
 
 static const int windSensorPin = A5;
 static const int TXPin = 4, RXPin = 3;
@@ -32,15 +35,30 @@ void setup() {
 void loop() {
   windSensorOutput = analogRead(windSensorPin);
   windDirection = map(windSensorOutput, 0,1006, 0, 359);
-
+  
 }
 
 float calulateHeadingToDestination(float latitude, float longitude, float destLatitude, float destLongitude) {
-  int headingToDestination;
+  latitude *= M_PI/180; //convert to radians
+  destLatitude *= M_PI/180; //convert to radians
+  float deltaLongitude = abs(destLongitude-longitude);
+  deltaLongitude *= M_PI/180; //convert to radians
 
+  float X = cos(targetLat) * sin(deltaLongitude);
+  float Y = cos(latitude) * sin(targetLat) - sin(latitude) * cos(targetLat) * cos(deltaLongitude);
+
+  float headingToDestination = atan2(X,Y);
+  headingToDestination *= 180/M_PI; //convert back to degrees
   return headingToDestination;
 }
 
-int calculateTargetHeading(int windDirection, float heading, float headingToDestination) {
+int calculateTargetHeading(int windDirection, float headingToDestination) {
+  int targetHeading;
 
+  return targetHeading;
+}
+
+int calculateRudderAngle(int targetHeading, float heading) {
+  int rudderAngle;
+  return rudderAngle;
 }
